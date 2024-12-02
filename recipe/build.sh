f8b4f7a65e463afd858a4b9c1ab6c24c2687b896
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-01 18:38:00 -0800

    enable rattler-build

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,6 +4,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 
 mkdir -p ${PREFIX}/bin
 mkdir -p ${PREFIX}/libexec/${PKG_NAME}
+ln -sf ${DOTNET_ROOT}/dotnet ${PREFIX}/bin
 
 # Build package with dotnet publish
 rm -rf global.json
@@ -16,6 +17,7 @@ tee ${PREFIX}/bin/coverlet << EOF
 #!/bin/sh
 exec \${DOTNET_ROOT}/dotnet exec \${CONDA_PREFIX}/libexec/coverlet/coverlet.console.dll "\$@"
 EOF
+chmod +x ${PREFIX}/bin/coverlet
 
 tee ${PREFIX}/bin/coverlet.cmd << EOF
 call %DOTNET_ROOT%\dotnet exec %CONDA_PREFIX%\libexec\coverlet\coverlet.console.dll %*
