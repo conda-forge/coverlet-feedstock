0c547e3da7583ae16364656654b34035868cd7af
Daniel Nachun <daniel.nachun@gmail.com>
2024-12-09 23:19:50 -0800

    fix bins (#4)

--- recipe/build.sh
+++ recipe/build.sh
@@ -25,3 +25,5 @@ EOF
 
 # Download dependency licenses wtih dotnet-project-licenses
 dotnet-project-licenses --input src/coverlet.console/coverlet.console.csproj -t -d license-files
+
+rm ${PREFIX}/bin/dotnet
